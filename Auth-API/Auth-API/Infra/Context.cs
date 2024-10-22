e041e55626166c57b43acc63ec6d83c36f0e1d7e
Luiz-Kohler <luizfelipekohler03@gmail.com>
2024-10-22 18:37:55 -0300

    add db context class for ef

---
+++ Auth-API/Auth-API/Infra/Context.cs
@@ -0,0 +1,26 @@
+﻿using Auth_API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Endpoint = Auth_API.Entities.Endpoint;
+
+namespace Auth_API.Infra
+{
+    public class Context : DbContext
+    {
+        public DbSet<Endpoint> Action { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<RoleEndpoint> RolesEndpoints { get; set; }
+        public DbSet<RoleUser> RolesUsers { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public Context(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+                relationship.DeleteBehavior = DeleteBehavior.Cascade;
+
+
+        }
+    }
+}
