ab82e74d2c2ab0fc3fdad06fc6ca029046e469b9
Luiz-Kohler <luizfelipekohler03@gmail.com>
2024-10-23 02:09:05 -0300

    creating all interfaces and implementations of repositories

---
+++ Auth-API/Auth-API/Repositories/IRepositories.cs
@@ -0,0 +1,13 @@
+﻿using Auth_API.Common;
+using Auth_API.Entities;
+using Endpoint = Auth_API.Entities.Endpoint;
+
+namespace Auth_API.Repositories
+{
+    public interface IEndpointRepository : IBaseEntityRepository<Endpoint> { }
+    public interface IProjectRepository : IBaseEntityRepository<Project> { }
+    public interface IRoleRepository : IBaseEntityRepository<Role> { }
+    public interface IRoleEndpointRepository : IBaseEntityRepository<RoleEndpoint> { }
+    public interface IRoleUserRepository : IBaseEntityRepository<RoleUser> { }
+    public interface IUserRepository : IBaseEntityRepository<User> { }
+}
---
+++ Auth-API/Auth-API/Repositories/Repositories.cs
@@ -0,0 +1,37 @@
+﻿using Auth_API.Common;
+using Auth_API.Entities;
+using Auth_API.Infra;
+using Endpoint = Auth_API.Entities.Endpoint;
+
+namespace Auth_API.Repositories
+{
+    public class EndpointRepository : BaseEntityRepository<Endpoint>, IEndpointRepository
+    {
+        public EndpointRepository(Context context) : base(context) { }
+    }
+
+    public class ProjectRepository : BaseEntityRepository<Project>, IProjectRepository
+    {
+        public ProjectRepository(Context context) : base(context) { }
+    }
+
+    public class RoleRepository : BaseEntityRepository<Role>, IRoleRepository
+    {
+        public RoleRepository(Context context) : base(context) { }
+    }
+
+    public class RoleEndpointRepository : BaseEntityRepository<RoleEndpoint>, IRoleEndpointRepository
+    {
+        public RoleEndpointRepository(Context context) : base(context) { }
+    }
+
+    public class RoleUserRepository : BaseEntityRepository<RoleUser>, IRoleUserRepository
+    {
+        public RoleUserRepository(Context context) : base(context) { }
+    }
+
+    public class UserRepository : BaseEntityRepository<User>, IUserRepository
+    {
+        public UserRepository(Context context) : base(context) { }
+    }
+}
