05f027a4761b24d3fb63cd6970386c93d84fa9f6
Luiz-Kohler <luizfelipekohler03@gmail.com>
2024-10-23 22:23:53 -0300

    creating user controller w/ create user endpoint

---
+++ Auth-API/Auth-API/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Auth_API.DTOs;
+using Auth_API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth_API.Controllers
+{
+    [Route("users")]
+    public class UsersController : Controller
+    {
+        private readonly IUserService _service;
+
+        public UsersController(IUserService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateUserRequest request)
+        {
+            await _service.Create(request);
+            return Ok();
+        }
+    }
+}
