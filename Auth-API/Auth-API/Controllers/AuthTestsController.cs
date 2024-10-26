396e41ee8ec6fbe782a9f5801fdcf54d8d3569d7
Luiz-Kohler <luizfelipekohler03@gmail.com>
2024-10-25 22:36:19 -0300

    creating auth endpoints for tests

---
+++ Auth-API/Auth-API/Controllers/AuthTestsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth_API.Controllers
+{
+    [Route("auth-tests")]
+    public class AuthTestsController : Controller
+    {
+
+        [HttpGet("no-auth-needed")]
+        public ActionResult NoAuthNeeded()
+        {
+            return Ok("^_~");
+        }
+
+        [Authorize]
+        [HttpGet("auth-needed")]
+        public ActionResult AuthNeeded()
+        {
+            return Ok("^_~");
+        }
+    }
+}
