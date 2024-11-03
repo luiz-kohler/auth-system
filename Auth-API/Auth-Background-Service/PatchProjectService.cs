825eba75b4c209042e2dc0788dede907059f090a
Luiz-Kohler <luizfelipekohler03@gmail.com>
2024-11-03 19:34:03 -0300

    implementing the background service structure

---
+++ Auth-API/Auth-Background-Service/PatchProjectService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Auth_Background_Service
+{
+    public class PatchProjectService : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("### Process starting ###");
+            ExecuteProcess();
+            return Task.CompletedTask;
+        }
+
+        private void ExecuteProcess()
+        {
+            Console.WriteLine("### Process executing ###");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("### Process stopping ###");
+            return Task.CompletedTask;
+        }
+    }
+}
